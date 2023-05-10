cee8b1a318703587837fa251203829bb28c5d678
alexeevch <106554488+alexeevch@users.noreply.github.com>
2023-05-10 22:57:41 +0300

    Подключил бота к nest и настроил окружение

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { StartModule } from './commands/start/start.module';
+
+import * as LocalSession from 'telegraf-session-local';
+import * as process from 'process';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TelegrafModule.forRoot({
+      include: [StartModule],
+      middlewares: [sessions.middleware()],
+      botName: process.env.BOT_NAME,
+      token: process.env.BOT_TOKEN,
+    }),
+    StartModule,
+  ],
 })
 export class AppModule {}
