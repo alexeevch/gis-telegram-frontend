a731cacc5441151d561f699d90c0d686b1a5a4df
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-05-11 14:52:09 +0300

    Вынес все необходимое в константы

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,1 +1,12 @@
-export const INFORMATION_SYSTEM_SCENE = 'information-system';
+export const BOT_NAME = 'MakeMakesBot';
+//SCENES
+export const MAIN_MENU_SCENE_TITLE = '📖 Главное меню';
+export const MAIN_MENU_SCENE_ID = 'main-menu';
+export const CATEGORY_QUESTION_SCENE_TITLE = '🔍 Вопросы по категориям';
+export const CATEGORY_QUESTION_SCENE_ID = 'information-system';
+export const QUESTION_SCENE_TITLE = 'Вопросы';
+export const QUESTION_SCENE_ID = 'question';
+export const GUIDE_SCENE_TITLE = '❓ Как пользоваться?';
+export const GUIDE_SCENE_ID = 'guide';
+export const FEEDBACK_SCENE_TITLE = '📞 Обратная связь';
+export const FEEDBACK_SCENE_ID = 'feedback';
